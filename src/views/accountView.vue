24cbc88ab9eaa469edffa128c0640bb2d3d5e86e
ASNK211 <ASNK2AA@GMAIL.com>
2023-01-12 08:51:35 +0200

    first commit

---
+++ src/views/accountView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "accountView",
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
+  props: {
+    name: String,
+    price: String,
+    pId: String,
+    serial: String,
+    image: String,
+    pp: String,
+  },
+  data() {
+    return {
+      item: {
+        productName: this.name,
+        productPrice: this.price,
+        productId: this.pId,
+        serial: this.serial,
+        image: this.image,
+        productQuanity: "" || this.pp,
+      },
+      alert: "",
+    };
+  },
+  methods: {
+    AddToCart() {
+      const Quanity = this.item.productQuanity;
+      if (Quanity <= 0) {
+        this.alert = "Quantity must be at least 1.";
+      } else {
+        this.alert = "";
+        this.$store.commit("updatetQuanity", this.item);
+      }
+    },
+  },
+};
+</script>
